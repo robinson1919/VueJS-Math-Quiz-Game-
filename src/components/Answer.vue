64d567bfb420e519788fa7b77b6fec10e2510d76
robinson1919 <46830749+robinson1919@users.noreply.github.com>
2020-04-10 05:54:48 -0400

    Second Commit

---
+++ src/components/Answer.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="alert alert-success text-center">
+        <h1>That's Correct!</h1>
+        <hr>
+        <button class="btn btn-primary" @click="onNextQuestion">Next Question</button>
+    </div>
+</template>
+<style>
+
+</style>
+<script>
+
+    export default{
+        methods: {
+            onNextQuestion() {
+                this.$emit('confirmed');
+            }
+        }
+    }
+</script>
+
+
+<style scoped>
+    .alert.alert-success {
+        transform: perspective(1000px)
+    }
+</style>
+    
+
